4a4a698e08d3b45f189c9f3f7a085c955c6c6cd4
terry <mingyteng@163.com>
2019-12-23 21:33:19 +0800

    [terry] sync

--- src/ReverseBits/Solution.java
+++ src/ReverseBits/Solution.java
@@ -9,7 +9,6 @@ package ReverseBits;
 public class Solution {
     public int reverseBits(int n) {
         int result = 0;
-        int mask = 1;
         for (int i = 0; i < 32; i++) {
             result <<= 1;
             result += ((n >> i) & 1);
