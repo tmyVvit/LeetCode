f4e980b1a13fd65ca3462fe18b871a0db840847f
terry <mingyteng@163.com>
2022-11-07 22:57:54 +0800

    leetcode: 779 k-th symbol in grammer

--- src/numberOfStudentsUnableToEatLunch/Solution.java
+++ src/numberOfStudentsUnableToEatLunch/Solution.java
@@ -54,7 +54,9 @@ public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
 
-        System.out.println(s.countStudents(new int[]{1, 1, 0, 0}, new int[]{0, 1, 0, 1}));
+//        System.out.println(s.countStudents(new int[]{1, 1, 0, 0}, new int[]{0, 1, 0, 1}));
+        System.out.println(0^1);
+        System.out.println(0^0);
     }
 
 
