ce1428ed243d047a9f9403ed7e593e70bb4e9db4
terry <mingyteng@163.com>
2023-02-19 13:21:29 +0800

    leetcode: 56 - merge intervals

--- src/mergeIntervals/Solution.java
+++ src/mergeIntervals/Solution.java
@@ -30,4 +30,23 @@ public class Solution {
          return result.toArray(new int[0][0]);
     }
 
+    public int[][] merge2(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt((int[] i) -> i[0]).thenComparingInt(i -> i[1]));
+        List<int[]> result = new ArrayList<>();
+        for (int i = 0; i < intervals.length;) {
+
+            int[] curr = intervals[i];
+            int endIdx = i + 1;
+            int maxEnd = curr[1];
+
+            while (endIdx < intervals.length && intervals[endIdx][0] <= maxEnd) {
+                maxEnd = Math.max(maxEnd, intervals[endIdx][1]);
+                endIdx++;
+            }
+            result.add(new int[]{curr[0], maxEnd});
+            i = endIdx;
+        }
+        return result.toArray(new int[0][0]);
+    }
+
 }
