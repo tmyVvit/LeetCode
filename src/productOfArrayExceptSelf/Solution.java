112871d7ae3540e420ccfd62f30dcbbafe9561d2
terry <mingyteng@163.com>
2023-02-22 22:50:35 +0800

    leetcode: 238 product of array except self

---
+++ src/productOfArrayExceptSelf/Solution.java
@@ -0,0 +1,20 @@
+package productOfArrayExceptSelf;
+
+public class Solution {
+    
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = nums[i-1] * res[i-1];
+        }
+
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] = res[i] * right;
+            right = right * nums[i];
+        }
+        return res;
+    }
+
+}
