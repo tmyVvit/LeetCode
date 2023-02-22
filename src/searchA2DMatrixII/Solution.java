33c1ffdbcd50790e8adcd421729862ade2cb4f68
terry <mingyteng@163.com>
2023-02-22 23:03:57 +0800

    leetcode: 240 search a 2d matrix

---
+++ src/searchA2DMatrixII/Solution.java
@@ -0,0 +1,19 @@
+package searchA2DMatrixII;
+
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            if (matrix[i][j] < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+    }
+}
