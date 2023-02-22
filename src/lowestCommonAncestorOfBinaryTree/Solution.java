7317c239a032836db41d9fdfa1e0ac70488ea0f4
terry <mingyteng@163.com>
2023-02-22 22:34:11 +0800

    leetcode: 236 lowest common ancestor of a binary tree

---
+++ src/lowestCommonAncestorOfBinaryTree/Solution.java
@@ -0,0 +1,28 @@
+package lowestCommonAncestorOfBinaryTree;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution {
+
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root.val == p.val || root.val == q.val) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) return right;
+        if (right == null) return left;
+        return root;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
