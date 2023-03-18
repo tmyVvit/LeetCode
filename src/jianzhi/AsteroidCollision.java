e9af4a1e708d6537fb198b8759930332feafc7a1
terry <mingyteng@163.com>
2023-03-18 10:22:23 +0800

    leetcode: 剑指 Offer II 037 小行星碰撞 https://leetcode.cn/problems/XagZNi/

---
+++ src/jianzhi/AsteroidCollision.java
@@ -0,0 +1,33 @@
+package jianzhi;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class AsteroidCollision {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new LinkedList<>();
+        for (int asteroid : asteroids) {
+            if (asteroid > 0) {
+                stack.addLast(asteroid);
+            } else {
+                while (!stack.isEmpty() && stack.peekLast() > 0 && stack.peekLast() + asteroid < 0) {
+                    stack.pollLast();
+                }
+
+                if (stack.isEmpty() || stack.peekLast() < 0) {
+                    stack.addLast(asteroid);
+                } else if (stack.peekLast() + asteroid == 0) {
+                    stack.pollLast();
+                }
+            }
+        }
+        int[] res = new int[stack.size()];
+        int i = 0;
+        while (!stack.isEmpty()) {
+            res[i++] = stack.pollFirst();
+        }
+        return res;
+    }
+}
