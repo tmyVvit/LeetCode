8247e0d82526927e8dff9ef584114b05ba243e33
terry <mingyteng@163.com>
2023-03-22 19:38:30 +0800

    PDD 二面 限流

---
+++ src/pdd/RateLimit.java
@@ -0,0 +1,33 @@
+package pdd;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class RateLimit {
+
+    private final int limit;
+    private final AtomicInteger count = new AtomicInteger(0);
+    private volatile long latestSeconds;
+
+    public RateLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public boolean rateLimit() {
+        long timeNow = System.currentTimeMillis();
+        long timeSecond = timeNow / 1000;
+
+        if (timeSecond > latestSeconds) {
+            synchronized (this) {
+                if (timeSecond > latestSeconds) {
+                    latestSeconds = timeSecond;
+                }
+            }
+        }
+        if (timeSecond == latestSeconds) {
+            int num = count.addAndGet(1);
+            return num <= limit;
+        }
+        return false;
+    }
+
+}
