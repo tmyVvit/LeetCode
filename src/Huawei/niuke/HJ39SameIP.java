f8ed8ccae2f923a70d31f1eb5a311d230829384d
terry <mingyteng@163.com>
2023-03-29 14:51:31 +0800

    牛客：华为 HJ39 判断两个IP是否属于同一子网

--- src/Huawei/niuke/HJ39SameIP.java
+++ src/Huawei/niuke/HJ39SameIP.java
@@ -2,57 +2,44 @@ package Huawei.niuke;
 
 import java.util.Scanner;
 
-public class HJ18FindValidIP {
+public class HJ39SameIP {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String maskStr = sc.next();
+            String ipStr0 = sc.next();
+            String ipStr1 = sc.next();
 
-        int a = 0,b = 0, c = 0, d = 0, e = 0, invalid = 0, pri = 0;
-        // 注意 hasNext 和 hasNextLine 的区别
-        while (in.hasNext()) { // 注意 while 处理多个 case
-            String[] line = in.nextLine().split("~");
-            String ip = line[0];
-            String mask = line[1];
+            int[] mask = validMask(maskStr);
+            if (mask == null) {
+                System.out.println(1);
+                continue;
+            }
 
-            int[] ip0 = validIP(ip);
+            int[] ip0 = validIP(ipStr0);
             if (ip0 == null) {
-                invalid++;
+                System.out.println(1);
                 continue;
             }
 
-            if (ip0[0] == 0 || ip0[0] == 127) {
+            int[] ip1 = validIP(ipStr1);
+            if (ip1 == null) {
+                System.out.println(1);
                 continue;
             }
 
-            int[] mask0 = validMask(mask);
-
-            if (mask0 == null) {
-                invalid++;
-                continue;
+            boolean same = true;
+            for (int i = 0; i < 4; i++) {
+                if ((ip0[i] & mask[i]) != (ip1[i] & mask[i])) {
+                    same = false;
+                    break;
+                }
             }
 
-            if (ip0[0] >= 1 && ip0[0] <= 126) {
-                a++;
-            } else if (ip0[0] >= 128 && ip0[0] <= 191) {
-                b++;
-            } else if (ip0[0] >= 192 && ip0[0] <= 223) {
-                c++;
-            } else if (ip0[0] >= 224 && ip0[0] <= 239) {
-                d++;
-            } else if (ip0[0] >= 240 && ip0[0] <= 255) {
-                e++;
-            }
-
-            if (ip0[0] == 10) {
-                pri++;
-            } else if (ip0[0] == 172 && ip0[1] >= 16 && ip0[1] <= 31) {
-                pri++;
-            } else if (ip0[0] == 192 && ip0[1] == 168) {
-                pri++;
-            }
+            System.out.println(same ? 0 : 2);
         }
 
-        System.out.printf("%d %d %d %d %d %d %d\n", a, b, c, d, e, invalid, pri);
     }
 
     private static int[] validIP(String ip) {
@@ -103,11 +90,6 @@ public class HJ18FindValidIP {
             }
         }
 
-        if ((nums[0] == 255 && nums[1] == 255 && nums[2] == 255 && nums[3] == 255)
-                ||(nums[0] == 0 && nums[1] == 0 && nums[2] == 0 && nums[3] == 0)) {
-            return null;
-        }
-
         if (!isPrefixAllOne(nums)) {
             return null;
         }
@@ -119,8 +101,12 @@ public class HJ18FindValidIP {
         boolean startZero = false;
 
         for (int num : nums) {
+            if (num < 0 || num > 255) {
+                return false;
+            }
+
             if (startZero) {
-                if (!(num == 0)) {
+                if (num != 0) {
                     return false;
                 }
             } else {
@@ -144,10 +130,8 @@ public class HJ18FindValidIP {
                     return false;
                 }
             }
-
         }
 
         return true;
     }
-
 }
