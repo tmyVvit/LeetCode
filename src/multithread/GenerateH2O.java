3802403259c2ecd3ba2b24780657c272bc24ccf2
mingyi.teng <mingyteng@163.com>
2023-05-09 20:16:40 +0800

    leetcode: 1117. H2O 生成 https://leetcode.cn/problems/building-h2o/

---
+++ src/multithread/GenerateH2O.java
@@ -0,0 +1,24 @@
+package multithread;
+
+import java.util.concurrent.Semaphore;
+
+public class GenerateH2O {
+
+    private final Semaphore s1 = new Semaphore(2);
+    private final Semaphore s2 = new Semaphore(1);
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        s2.acquire();
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        releaseHydrogen.run();
+        s1.release();
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        s1.acquire(2);
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+        releaseOxygen.run();
+        s2.release(2);
+    }
+
+}
--- src/multithread/ZeroEvenOdd.java
+++ src/multithread/ZeroEvenOdd.java
@@ -1,4 +1,4 @@
-package MultiThread;
+package multithread;
 
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
