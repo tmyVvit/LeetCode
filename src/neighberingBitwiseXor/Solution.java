6031987a788f782a48aa05f8aa047a55a90bd0cc
mingyi.teng <mingyteng@163.com>
2023-08-17 00:06:38 +0800

    leetcode: 2683. 相邻值的按位异或 https://leetcode.cn/problems/neighboring-bitwise-xor/

---
+++ src/neighberingBitwiseXor/Solution.java
@@ -0,0 +1,34 @@
+package neighberingBitwiseXor;
+
+// 2683. 相邻值的按位异或 https://leetcode.cn/problems/neighboring-bitwise-xor/
+public class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        if (derived.length == 1) {
+            return derived[0] == 0;
+        }
+
+        int[] origin = new int[derived.length];
+        int[][] p1 = new int[][]{{0, 0},{1, 1}};
+        int[][] p2 = new int[][]{{0, 1},{1, 0}};
+
+        int[][] p0 = derived[0] == 0 ? p1 : p2;
+
+        for (int[] p : p0) {
+            origin[0] = p[0];
+            origin[1] = p[1];
+
+            if (valid(derived, origin)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean valid(int[] derived, int[] origin) {
+        int len = derived.length;
+        for (int i = 1; i < len - 1; i++) {
+            origin[i + 1] = origin[i] ^ derived[i];
+        }
+        return (origin[0] ^ origin[len - 1]) == derived[len - 1];
+    }
+}
