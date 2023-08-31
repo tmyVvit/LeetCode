0b8870d28d0881f947e1174ad3a8ccd0b78c0ceb
tmyVvit <terryteng0106@gmail.com>
2023-09-01 00:20:07 +0800

    20230830 leetcode 20230831 leetcode

---
+++ src/a_day20230830/Bytedance.java
@@ -0,0 +1,55 @@
+package a_day20230830;
+
+import java.util.HashSet;
+
+public class Bytedance {
+
+    public static void main(String[] args) {
+
+        Node node = buildList(10);
+        System.out.println(play(node));
+    }
+
+    static int play(Node node) {
+        Node curr = node;
+        Node next = curr.next;
+        int i = 2;
+        while (curr.val != next.val) {
+            // curr
+            if (i != 3) {
+                i++;
+                curr = next;
+                next = curr.next;
+            } else {
+                if (next.next.val == curr.val) {
+                    return curr.val;
+                }
+                curr.next = next.next;
+                next = curr.next;
+                i = 1;
+            }
+        }
+        return curr.val;
+    }
+
+    static Node buildList(int n) {
+        Node head = new Node();
+        Node curr = head;
+        for (int i = 1; i <= n; i++) {
+            Node node = new Node();
+            node.val = i;
+            curr.next = node;
+            curr = node;
+        }
+        curr.next = head.next;
+        return head.next;
+    }
+
+
+    static class Node {
+        int val;
+        Node next;
+    }
+
+}
+
---
+++ src/a_day20230831/MinimumJumpsToReachHome.java
@@ -0,0 +1,61 @@
+package a_day20230831;
+
+import java.util.*;
+
+public class MinimumJumpsToReachHome {
+    public int minimumJumps(int[] forbidden, int a, int b, int x) {
+        // int len = x + b + 1;
+        Set<Integer> forbs = new HashSet<>();
+        int maxF = -1;
+        for (int f : forbidden) {
+            forbs.add(f);
+            maxF = Math.max(f, maxF);
+        }
+        if (x == 0) {
+            return forbs.contains(0) ? -1 : 0;
+        }
+
+
+        int len = Math.max(x + b, maxF + a + b) + 1;
+        int[][] dp = new int[len][2];
+
+
+        List<int[]> list0 = new ArrayList<>();
+        list0.add(new int[]{0, 0});
+        int step = 1;
+        while(!list0.isEmpty()) {
+            List<int[]> curr0 = list0;
+            list0 = new ArrayList<>();
+
+            for (int[] idx : curr0) {
+                int next0 = idx[0] + a;
+                if (next0 < len && !forbs.contains(next0)) {
+                    if (next0 == x) {
+                        return step;
+                    }
+
+                    if (dp[next0][0] == 0) {
+                        dp[next0][0] = step;
+                        list0.add(new int[]{next0, 0});
+                    }
+                }
+
+                if (idx[1] == 0) {
+                    int next1 = idx[0] - b;
+                    if (next1 > 0 && !forbs.contains(next1)) {
+                        if (next1 == x) {
+                            return step;
+                        }
+                        if (dp[next1][0] == 0 && dp[next1][1] == 0) {
+                            dp[next1][1] = step;
+                            list0.add(new int[]{next1, 1});
+                        }
+
+                    }
+                }
+            }
+            step++;
+        }
+        return -1;
+    }
+}
